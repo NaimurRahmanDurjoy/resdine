a20ca2f6867bea04050a4fa0264b69736f9ae860
Md. Naimur Rahman <75179160+NaimurRahmanDurjoy@users.noreply.github.com>
2026-07-16 15:04:18 +0600

    For Testing Deploy migrate fresh

--- deploy.sh
+++ deploy.sh
@@ -7,7 +7,7 @@ php artisan optimize
 php artisan storage:link --force
 
 # Run database migrations automatically on every successful deploy
-php artisan migrate --force
+php artisan migrate:fresh --force
 
 # Start Supervisor to run both Apache and Laravel Reverb together
 exec supervisord -c /var/www/html/supervisor.conf
